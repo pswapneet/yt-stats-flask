8dcbc7d8d59f26d8f805973afe794940ab08e8d8
pswapneet <pswapneet@gmail.com>
2023-02-01 09:27:38 +0100

    ok

---
+++ static/js/layout.js
@@ -0,0 +1,7 @@
+function showDiv(divId) {
+    document.getElementById(divId).style.display = "block";
+  }
+  
+  function hideDiv(divId) {
+    document.getElementById(divId).style.display = "none";
+  }  
